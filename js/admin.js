0a215b7302a08a140665f50e4722a001f5208def
abdulsalamgellezsgca <asalamalameen@gmail.com>
2026-01-16 00:08:00 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,16 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged, signOut } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+onAuthStateChanged(auth, (user) => {
+  if (!user || user.email !== "asalamalameen@gmail.com") {
+    window.location.href = "index.html";
+  }
+});
+
+// LOGOUT
+document.querySelector("button:last-child").addEventListener("click", () => {
+  signOut(auth).then(() => {
+    window.location.href = "index.html";
+  });
+});
