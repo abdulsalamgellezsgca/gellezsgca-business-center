d8ea2f1db37fd79abd01314c0fd3d8f7a9006fec
abdulsalamgellezsgca <asalamalameen@gmail.com>
2026-01-15 23:14:24 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// 🔴 FIREBASE CONFIG (PASTE YOURS BELOW)
+PASTE_YOUR_FIREBASE_CONFIG_HERE
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
