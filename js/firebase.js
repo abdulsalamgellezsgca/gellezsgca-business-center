ac2322feadcce9320223c0f3da4df3bfe1fa08cd
abdulsalamgellezsgca <asalamalameen@gmail.com>
2026-01-15 23:15:33 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,8 +3,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 // 🔴 FIREBASE CONFIG (PASTE YOURS BELOW)
-PASTE_YOUR_FIREBASE_CONFIG_HERE
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCdSMaU4yIPL0VKnD0G9bQ_ojyFbJtmVvM",
+  authDomain: "gellezsgca-business-cent-44a59.firebaseapp.com",
+  projectId: "gellezsgca-business-cent-44a59",
+  storageBucket: "gellezsgca-business-cent-44a59.firebasestorage.app",
+  messagingSenderId: "264543270872",
+  appId: "1:264543270872:web:a43cf64eaa5c092b2664ad"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
