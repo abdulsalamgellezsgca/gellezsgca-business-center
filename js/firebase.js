86d1b3518a971dc46d20bb115a033642a9470684
abdulsalamgellezsgca <asalamalameen@gmail.com>
2026-01-16 01:44:14 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,18 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// 🔴 FIREBASE CONFIG (PASTE YOURS BELOW)
 const firebaseConfig = {
   apiKey: "AIzaSyCdSMaU4yIPL0VKnD0G9bQ_ojyFbJtmVvM",
   authDomain: "gellezsgca-business-cent-44a59.firebaseapp.com",
   projectId: "gellezsgca-business-cent-44a59",
-  storageBucket: "gellezsgca-business-cent-44a59.firebasestorage.app",
+  storageBucket: "gellezsgca-business-cent-44a59.appspot.com",
   messagingSenderId: "264543270872",
   appId: "1:264543270872:web:a43cf64eaa5c092b2664ad"
 };
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-// Export Firebase services
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
