342f0fa082ffe58b147e0ee106876c3a94a05f08
abdulsalamgellezsgca <asalamalameen@gmail.com>
2026-01-16 01:50:12 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,16 +1,20 @@
-import { auth } from "./firebase.js";
-import { onAuthStateChanged, signOut } from
-  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { db } from "./firebase.js";
+import {
+  collection, getDocs, updateDoc, doc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-onAuthStateChanged(auth, (user) => {
-  if (!user || user.email !== "asalamalameen@gmail.com") {
-    window.location.href = "index.html";
-  }
-});
+const snap = await getDocs(collection(db, "transactions"));
 
-// LOGOUT
-document.querySelector("button:last-child").addEventListener("click", () => {
-  signOut(auth).then(() => {
-    window.location.href = "index.html";
-  });
+snap.forEach(t => {
+  const d = t.data();
+  const div = document.createElement("div");
+  div.innerHTML = `
+    ${d.service} - ₦${d.amount} - ${d.status}
+    <button>Approve</button>
+  `;
+  div.querySelector("button").onclick = async () => {
+    await updateDoc(doc(db, "transactions", t.id), { status: "approved" });
+    location.reload();
+  };
+  txList.appendChild(div);
 });
